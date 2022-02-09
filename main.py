e637e735300af668bb78333745f5d67eccd4d9ac
siegfer <nicksalguerobui@icloud.com>
2022-02-08 21:39:04 -0800

    Update parameters

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Something'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs 
+    # adding additional sort paramenters with default value of none
+    # give default value to limit & published
+    # paramenters is /blog?limit=10&published=true/false
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    return {'data':  {'1', '2'}}
+
+@app.post('/blog')
+def create_blog():
+    return {'data': 'Blog is created'}
