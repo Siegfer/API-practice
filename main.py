deddec28e450ff0f46b5260826920c9acac27096
siegfer <nicksalguerobui@icloud.com>
2022-02-08 18:47:19 -0800

    Daily update

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Item(BaseModel):
 # PUT
 # DELETE
 
-inventory = {1: {"name": "Milk", "price": 3.99, "brand": "Regular"}}
+inventory = {}
 
 
 @app.get("/")
@@ -32,23 +32,23 @@ def about():
 @app.get("/get-item/{item_id}")
 def get_item(
     item_id: int = Path(
-        None, description="The ID of the item you like to view.", gt=0, lt=2
+        None, description="The ID of the item you like to view.", gt=0
     )
 ):
     return inventory[item_id]
 
 
 @app.get("/get-by-name/{item_id}")
-def get_item(*, name: Optional[str] = None, test: int):
+def get_item(*, item_id: int, name: Optional[str] = None, test: int):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data": "Not found"}
-
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
     if item_id in inventory:
-        return {"Error": "Item ID already exists."}
-
-    inventory[item_id] = {"name": item.name, "brand": item.brand, "price": item.price}
+        return {"Error": "Item ID already exists"}
+    
+    inventory[item_id] = item
+    return inventory[item_id]
